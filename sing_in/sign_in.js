db96bdcf42beba91f07079a223e9260376ebc314
bjsaimani <45102237+bjsaimani@users.noreply.github.com>
2022-09-10 10:27:56 +0530

    adding expenses of users

--- sing_in/sign_in.js
+++ sing_in/sign_in.js
@@ -1,4 +1,33 @@
 
+
+
+window.addEventListener('DOMContentLoaded',displaycontent());
+
+function displaycontent(){
+    const token = localStorage.getItem('token');
+
+    const profile_container = document.getElementById('profile_container');
+
+    const sign_in_container = document.getElementById('sign_in_container');
+
+    if(!token){
+        profile_container.classList.add('active')// remove this container
+        sign_in_container.classList.add('active');
+    }
+    else{
+        if(sign_in_container.classList.contains('active')){
+            sign_in_container.classList.remove('active')
+        }
+        // displays the profile container   
+        if(profile_container.classList.contains('active')){
+            profile_container.classList.remove('active')
+        }
+    }
+}
+
+
+
+//document.getElementById('expenses_main_container').classList.add('active');
 const sign_in = document.getElementById('sign_in');
 
 sign_in.addEventListener('click', (e) =>{
@@ -12,14 +41,40 @@ sign_in.addEventListener('click', (e) =>{
     }
     
     if(email && password){
+        console.log(obj)
         axios.post(`http://localhost:4000/sign-in`,obj)   // if we try to send the obj through get, we need to stringify that obj. then only we can parse that object and use it.
         .then(result =>{
             if(result.data.msg){
                 console.log(result);
                 window.alert(result.data.msg);
+                // store the token in the local storage for further refences
+                localStorage.setItem('token',result.data.token);
+                displaycontent();
             }
         }).catch(err => console.log(err));  
     }else{
         window.alert('pls check your credentials');
     }
+})
+
+// adding expenses
+document.getElementById('add_expenses_btn').addEventListener('click',(e)=>{
+    e.preventDefault();
+    const event = document.getElementById('event').value;
+    const price = document.getElementById('price').value;
+
+    const obj = {
+        event:event,
+        price:price
+    };
+
+    const token = localStorage.getItem('token');
+    console.log(token)
+    // send this data to backend
+    axios.post(`http://localhost:4000/add-expenses`,obj,{headers:{"authorization":token} })
+    .then(result =>{
+        window.alert(result.data.msg)
+    })
+    .catch(err => console.log(err));
+    
 })
