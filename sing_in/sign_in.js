780d7fbb82d67260973bb02c7f6437753bd3239f
bjsaimani <45102237+bjsaimani@users.noreply.github.com>
2022-09-16 11:19:17 +0530

    adding payment option

--- sing_in/sign_in.js
+++ sing_in/sign_in.js
@@ -78,3 +78,65 @@ document.getElementById('add_expenses_btn').addEventListener('click',(e)=>{
     .catch(err => console.log(err));
     
 })
+
+// paying the money
+document.getElementById('pay_btn').onclick = async function(e){
+    e.preventDefault();
+
+    const token = localStorage.getItem('token');
+    console.log(token);
+    let response;
+    await axios.get(`http://localhost:4000/purchase/membership`,{headers:{"authorization":token}})
+    .then(res=> response=res)
+    .catch(err => console.log(err));    
+    console.log(response);
+            
+
+    const options = {
+        "key": response.data.key_id, // Enter the Key ID generated from the Dashboard
+        "name": "Acme Corp",
+        "description": "Test Transaction",
+        "order_id": response.data.order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+        "prefill": {
+                        "name": "Test User",
+                        "email": "test.user@example.com",
+                        "contact": "7003442036"
+                    },
+        "theme": {
+            "color": "#3399cc"
+        },
+        // post the payment id on payment success event in server
+        "handler": function (pymnt_success){
+            alert(pymnt_success.razorpay_payment_id);
+            alert(pymnt_success.razorpay_order_id);
+            alert(pymnt_success.razorpay_signature);
+
+            // posting the payment id in server
+            axios.post('http://localhost:4000/purchase/membershipStatus',{
+                orderId:options.order_id,
+                payment_id: pymnt_success.razorpay_payment_id
+            },{
+                headers:{"authorization":token}
+            })
+            .then(() =>{
+                window.alert('you are premium user now');
+            }).catch(err =>alert('something went wrong'));
+        }
+    }
+
+    // initialize payment process with options
+    const rzpy = new Razorpay(options);    // declearing and initialze the razorpay instance 
+    rzpy.open()                     // starting the payment process
+
+    // handle the payment failure scenarios
+    rzpy.on('payment.failed', function (response){
+        alert(response.error.code);
+        alert(response.error.description);
+        alert(response.error.source);
+        alert(response.error.step);
+        alert(response.error.reason);
+        alert(response.error.metadata.order_id);
+        alert(response.error.metadata.payment_id);
+    });
+
+}
